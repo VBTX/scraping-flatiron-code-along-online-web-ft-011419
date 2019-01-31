3c08b26b564abe3f8b852fc25ee84eb792bbc3e5
Victoria B <viktorija.bourassa@gmail.com>
2019-01-31 16:14:45 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,7 +2,10 @@ class Course
 
   attr_accessor :title, :description, :schedule
   @@all = []
-
+  def initialize(title = nil, description = nil, schedule = nil)
+    @title = title
+    @decription = description
+    @schedule = schedule
   def self.all
     @@all
   end
